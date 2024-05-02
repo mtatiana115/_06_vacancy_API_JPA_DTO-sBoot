5d4e216876bcee0e0c2e8a5f223fbf85976a4f49
mtatiana115 <mtatiana115@gmail.com>
2024-05-02 09:29:27 -0500

    fix/deletemapping in vacancyController

--- src/main/java/com/riwi/vacancies/controllers/CompanyController.java
+++ src/main/java/com/riwi/vacancies/controllers/CompanyController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -49,7 +50,7 @@ public class CompanyController {
       return ResponseEntity.ok(this.iCompanyService.getById(id));
     }
 
-  @GetMapping(path = "/{id}")
+  @DeleteMapping(path = "/{id}")
   public ResponseEntity<Void> delete(
     @PathVariable String id){
       this.iCompanyService.delete(id);
